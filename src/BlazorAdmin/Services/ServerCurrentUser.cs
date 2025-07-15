996239f4ec53b3ffb3ef330d6d26f4bdccd67bd8
minh.vu <minhvu.box@gmail.com>
2025-07-16 04:03:22 +0700

    move FluentBlazor to other branch

--- src/BlazorAdmin/Services/ServerCurrentUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Monolith;
-using System.Security.Claims;
-
-namespace BlazorAdmin.Services;
-
-public class ServerCurrentUser(IHttpContextAccessor httpContextAccessor) : CurrentUserBase, ICurrentUser
-{
-    protected override ClaimsPrincipal? User => httpContextAccessor.HttpContext?.User;
-}
