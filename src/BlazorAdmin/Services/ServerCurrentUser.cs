acf5e79bb94adf07d90b1300213591d2aa5680d0
minh.vu <minhvu.box@gmail.com>
2025-07-15 21:49:41 +0700

    add blazor UI

---
+++ src/BlazorAdmin/Services/ServerCurrentUser.cs
@@ -0,0 +1,9 @@
+﻿using Monolith;
+using System.Security.Claims;
+
+namespace BlazorAdmin.Services;
+
+public class ServerCurrentUser(IHttpContextAccessor httpContextAccessor) : CurrentUserBase, ICurrentUser
+{
+    protected override ClaimsPrincipal? User => httpContextAccessor.HttpContext?.User;
+}
