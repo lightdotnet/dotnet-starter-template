e891138851c8e2e1ee622bcb78f409dd0b551162
minh.vu <minhvu.box@gmail.com>
2025-07-23 12:09:17 +0700

    add BlazorApp

---
+++ src/BlazorApp/BlazorApp/ConfigureExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Light.AspNetCore.Middlewares;
+using Light.Mediator;
+using Monolith.BlazorApp.Services;
+using Monolith.Identity;
+using Monolith.Identity.Notifications.SignalR;
+using System.Reflection;
+
+namespace Monolith.BlazorApp;
+
+public static class ConfigureExtensions
+{
+    private static readonly Assembly[] assemblies =
+        [
+            typeof(Program).Assembly,
+            typeof(IdentityModule).Assembly,
+            typeof(SignalRModule).Assembly,
+        ];
+
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Light Framework
+        services.AddMediatorFromAssemblies(assemblies);
+        services.AddBehaviors(typeof(ValidationBehaviour<,>));
+        services.AddOptions<RequestLoggingOptions>().BindConfiguration("RequestLogging");
+
+        services.AddInfrastructureServices();
+        services.AddHealthChecks();
+
+        services.AddHttpContextAccessor();
+        services.AddScoped<ICurrentUser, ServerCurrentUser>();
+        services.AddPermissions();
+
+        return services;
+    }
+}
---
+++ src/BlazorApp/BlazorApp/Program.cs
@@ -0,0 +1,74 @@
+global using Light.Identity.Models;
+global using Monolith.Identity.Data;
+using Monolith.BlazorApp.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Monolith.BlazorApp;
+using Monolith.BlazorApp.Components.Account;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents()
+    .AddAuthenticationStateSerialization();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<Role>()
+    .AddEntityFrameworkStores<AppIdentityDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<User>, IdentityNoOpEmailSender>();
+
+builder.Services.ConfigureServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Monolith.BlazorApp.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
