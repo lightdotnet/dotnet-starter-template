ad4eda75c1d806ebe4c74369890bc020a62326d6
minh.vu <minhvu.box@gmail.com>
2025-07-25 20:14:16 +0700

    update Logging

--- src/ClientApp/BlazorWasm/Program.cs
+++ src/ClientApp/BlazorWasm/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// OR target specific categories
+/*
+builder.Logging.AddFilter("System.Net.Http.HttpClient", LogLevel.Warning);
+builder.Logging.AddFilter("Microsoft.AspNetCore.Components.WebAssembly.Http.WebAssemblyHttpMessageHandler", LogLevel.Warning);
+*/
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddServices(builder.Configuration);
