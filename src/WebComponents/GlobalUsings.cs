5d2c747c5bf546536120e7f0a3fe31e2da616151
minh.vu <minhvu.box@gmail.com>
2025-10-14 08:30:19 +0700

    fix sln

--- src/WebComponents/GlobalUsings.cs
+++ src/WebComponents/GlobalUsings.cs
@@ -1,4 +1,1 @@
-﻿global using Light.Extensions.DependencyInjection;
-global using Light.Identity;
-global using Microsoft.Extensions.Logging;
-global using Monolith;
+﻿global using Microsoft.Extensions.Logging;
