6b6d3bc1dc042cdcd8aa9dc9fdb75e400c490a6b
minhvu <vd.minh@outlook.com>
2026-07-29 15:16:24 +0700

    vendor: update nuget

--- src/Shared/Entities.cs
+++ src/Shared/Entities.cs
@@ -2,11 +2,11 @@
 
 namespace Monolith;
 
-public abstract class AuditableEntity<T> : Light.Domain.Entities.AuditableEntity<T>;
+public abstract class AuditableEntity<T> : Light.Domain.Entities.BaseAuditableEntity<T>;
 
 public abstract class AuditableEntity : AuditableEntity<string>
 {
     protected AuditableEntity() => Id = LightId.NewId();
 }
 
-public abstract record DomainEvent : Light.Domain.Entities.DomainEvent, Light.Mediator.INotification;
+public abstract record DomainEvent : Light.Domain.Entities.BaseEvent, Light.Mediator.INotification;
